314a9a584353a11c8fd2c0a619b7e5c6ecae644b
Ashleen Rath <ashleen.rath@hotmail.com>
2022-03-21 10:48:48 +0000

    User model changes and GPG signed commits

---
+++ models/user.model.js
@@ -0,0 +1,10 @@
+class User {
+  constructor(email, password, fullname, street, eircode, county) {
+    (this.email = email),
+      (this.password = password),
+      (this.fullname = fullname),
+      (this.street = street),
+      (this.eircode = eircode),
+      (this.county = county);
+  }
+}
