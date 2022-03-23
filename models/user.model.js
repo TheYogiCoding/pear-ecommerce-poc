dadfb802733fe3ef741d5c9bdbab17705a9c6757
Ashleen Rath <ashleen.rath@hotmail.com>
2022-03-23 16:49:03 +0000

    User model changes - installed bcryptjs for hashing passwords

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,23 @@
+const db = require("../data/database");
+
 class User {
   constructor(email, password, fullname, street, eircode, county) {
     (this.email = email),
       (this.password = password),
       (this.fullname = fullname),
-      (this.street = street),
-      (this.eircode = eircode),
-      (this.county = county);
+      (this.address = {
+        street: street,
+        eircode: eircode,
+        county: county,
+      });
+  }
+
+  signUp() {
+    db.getDb().collection("users").insertOne({
+      email: this.email,
+      password: this.password,
+      name: this.name,
+      address: this.address,
+    });
   }
 }
