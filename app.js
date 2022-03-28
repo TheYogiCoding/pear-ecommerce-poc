80ecbb1cbde5083faf3cc0102943fd92af585aaa
Ashleen Rath <ashleen.rath@hotmail.com>
2022-03-28 15:23:51 +0100

    Implement CSRF Token Protection in forms

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const path = require("path");
 const express = require("express");
 
+const csrf = require("csurf");
+
 const authRoutes = require("./routes/auth.routes");
 
 const db = require("./data/database");
+
+const csurf = require("csurf");
+const { addCsrfToken } = require("./middlewares/csrf-token");
+
 const app = express();
 
 //Make express aware of views folder and
@@ -14,6 +20,10 @@ app.set("views", path.join(__dirname, "views"));
 //Serve static css files
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(csurf());
+
+app.use(addCsrfToken);
 
 app.use(authRoutes);
 
---
+++ middlewares/csrf-token.js
@@ -0,0 +1,8 @@
+function addCsrfToken(req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+}
+
+module.exports = {
+  addCsrfToken: addCsrfToken,
+};
