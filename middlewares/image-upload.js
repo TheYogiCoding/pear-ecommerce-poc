0ffb653d8f47244d0a7ce5342d835527017b6771
Ashleen Rath <ashleen.rath@hotmail.com>
2022-03-30 12:44:11 +0100

    Multer middleware - troubleshooting req.body null

---
+++ middlewares/image-upload.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+const uuid = require("uuid").v4;
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "product-data/images",
+    filename: function (req, file, cb) {
+      cb(null, uuid() + "-" + file.originalname);
+    },
+  }),
+});
+
+//name image in new-product.ejs
+const configuredMulterMiddleware = upload.single("image");
+
+module.exports = configuredMulterMiddleware;
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
 const adminController = require("../controllers/admin.controller");
+const imageUploadMiddleware = require("../middlewares/image-upload");
 
 const router = express.Router();
 
 router.get("/products", adminController.getProducts);
 
 router.get("/products/new", adminController.getNewProduct);
+
+router.post(
+  "/products",
+  imageUploadMiddleware,
+  adminController.createNewProduct
+);
 
 module.exports = router;
