c89c80d27d8d2246ee07f2fae3dc5546e25b725d
Ashleen Rath <ashleen.rath@hotmail.com>
2022-04-07 12:04:35 +0100

    Updating the shopping cart and page

--- models/cart.model.js
+++ models/cart.model.js
@@ -16,8 +16,8 @@ class Cart {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
       if (item.product.id === product.id) {
-        cartItem.quantity += 1;
-        cartItem.price += product.price;
+        cartItem.quantity = +item.quantity + 1;
+        cartItem.totalPrice = item.totalPrice + product.price;
         this.items[i] = cartItem;
 
         this.totalQuantity++;
